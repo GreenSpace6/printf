ac506f7adee3db43b1f9f4d82c6803a1c3596290
THANDA <powerwolf.tj@gmail.com>
2023-08-22 18:33:16 -0700

    Updated

--- main.h
+++ main.h
@@ -7,19 +7,20 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* OUR FLAGS */
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* THE SIZES */
+/* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
  * struct fmt - Struct op
+ *
  * @fmt: The format.
  * @fn: The function associated.
  */
@@ -29,8 +30,10 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
+
 /**
  * typedef struct fmt fmt_t - Struct op
+ *
  * @fmt: The format.
  * @fm_t: The function associated.
  */
@@ -40,7 +43,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/*** THE FUNCTIONS ***/
+/****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -71,11 +74,11 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Function to print memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
@@ -103,7 +106,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/*** THE UTILS ******/
+/****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
