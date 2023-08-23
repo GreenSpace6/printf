aa618ade5516b7f2416a451204e4e13027d881b4
THANDA <powerwolf.tj@gmail.com>
2023-08-22 17:24:09 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,67 +1,45 @@
+#include <stdio.h>
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf - Printf function
- * by Thanda and Nomore
- * @format: format.
- * Return: Printed chars.
- */
-int _printf(const char *format, ...)
+int main(void)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	int len = 0;
+	int len2 = 0;
+	
+	len = _printf("A %s sentence\n", "simple");
+	len2 = printf("A %s sentence\n", "simple");
+	printf("Len:[%d]\n", len);
+	printf("Len1:[%d]\n\n", len2);
+	
+	len = _printf("A %c%c%s sentence\n", 's', 'i', "mple");
+	len2 = printf("A %c%c%s sentence\n", 's', 'i', "mple");
+	printf("Len:[%d]\n", len);
+	printf("Len1:[%d]\n\n", len2);
 
-	if (format == NULL)
-		return (-1);
+	len = _printf("A simple %v entence\n");
+	len2 = printf("A simple %v entence\n");
+	printf("Len:[%d]\n", len);
+	printf("Len1:[%d]\n\n", len2);
 
-	va_start(list, format);
+	len = _printf("Percentage: %%\n");
+	len2 = printf("Percentage: %%\n");
+	printf("Len:[%d]\n", len);
+	printf("Len1:[%d]\n\n", len2);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+	len = _printf("NULL: %c, %s\n", NULL, NULL);
+	len2 = printf("NULL: %c, %s\n", NULL, NULL);
+	printf("Len:[%d]\n", len);
+	printf("Len1:[%d]\n\n", len2);
 
-	print_buffer(buffer, &buff_ind);
+	len = _printf("%");
+	len2 = printf("%");
+	printf("Len:[%d]\n", len);
+	printf("Len1:[%d]\n\n", len2);
 
-	va_end(list);
+	len = _printf("A simple sentence%");
+	len2 = printf("\nA simple sentence%");
+	printf("\nLen:[%d]\n", len);
+	printf("Len1:[%d]\n\n", len2);	
 
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	return (0);
 }
